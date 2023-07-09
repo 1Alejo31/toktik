616f04dd05906babd48bb43a647dfc4ff4fe0020
alejo Gonzalez <alejo.g31@hotmail.com>
2023-07-09 09:39:02 -0500

    Usando el repositorio y el origen de datos

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tok_tik/config/theme/app_theme.dart';
+import 'package:tok_tik/infrastructure/datasources/local_video_datasource_impl.dart';
+import 'package:tok_tik/infrastructure/repositories/video_posts_repository_impl.dart';
 import 'package:tok_tik/presentation/screen/discover/dicover_screen.dart';
 import 'package:tok_tik/providers/discover_provider.dart';
 
@@ -11,12 +13,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final videoPostsRepository =
+        VideoPostsRepositoryImpl(videosDataSource: LocalVideoDatasourceImpl());
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
             lazy: false,
             //Operador de cascada ..loadNextPage()
-            create: (_) => DiscoverProvider()..loadNextPage()),
+            create: (_) =>
+                DiscoverProvider(videoPostsRepository: videoPostsRepository)
+                  ..loadNextPage()),
       ],
       child: MaterialApp(
           title: 'TokTik',
--- lib/providers/discover_provider.dart
+++ lib/providers/discover_provider.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:tok_tik/domain/entities/video_post.dart';
-import 'package:tok_tik/infrastructure/models/local_video_model.dart';
-import 'package:tok_tik/shared/data/local_video_post.dart';
+import 'package:tok_tik/domain/repositories/video_post_repository.dart';
 
 class DiscoverProvider extends ChangeNotifier {
+  final VideoPostsRepository videoPostsRepository;
+
   bool initialLoading = true;
   List<VideoPost> videos = [];
+
+  DiscoverProvider({
+    required this.videoPostsRepository,
+  });
 
   Future<void> loadNextPage() async {
     //Simulacion de comunicacion http
@@ -13,10 +18,10 @@ class DiscoverProvider extends ChangeNotifier {
     await Future.delayed(const Duration(seconds: 2));
 
     //Carga los videos
-    final List<VideoPost> newVideos = videoPosts
+    /*final List<VideoPost> newVideos = videoPosts
         .map((video) => LocalVodeoModel.fromJson(video).toVideoEntity())
         .toList();
-
+    */
     //final List<VideoPost> newVideos = videoPosts
     //    .map((video) => VideoPost(
     //        captoin: video['name'],
@@ -24,6 +29,8 @@ class DiscoverProvider extends ChangeNotifier {
     //        likes: video['likes'],
     //        views: video['views']))
     //    .toList();
+
+    final newVideos = await videoPostsRepository.getTrendingVideosByPage(1);
 
     videos.addAll(newVideos);
 
